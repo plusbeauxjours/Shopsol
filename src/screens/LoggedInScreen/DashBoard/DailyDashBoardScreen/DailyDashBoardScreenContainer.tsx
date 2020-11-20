7b346a329c33375466cb4c5f963fea6c8e123fe0
plusbeauxjours <plusbeauxjours@gmail.com>
2020-11-20 14:58:49 +0700

    SET DashBoardScreen DailyDataCard

--- src/screens/LoggedInScreen/DashBoard/DailyDashBoardScreen/DailyDashBoardScreenContainer.tsx
+++ src/screens/LoggedInScreen/DashBoard/DailyDashBoardScreen/DailyDashBoardScreenContainer.tsx
@@ -9,8 +9,18 @@ export default () => {
   const {CALENDAR_DATA} = useSelector((state: any) => state.calendarReducer);
   const {EMPLOYEE_LIST} = useSelector((state: any) => state.employeeReducer);
 
-  const [empList, setEmpList] = useState<any>([]);
-  const [allEmpWorkingTimeAvg, setAllEmpWorkingTimeAvg] = useState<number>(0);
+  const [EMP_LIST, setEMP_LIST] = useState<any>([]);
+  const [totalEARLY, setTotalEARLY] = useState<number>(0);
+  const [EARLY_EMP_LIST, setEARLY_EMP_LIST] = useState<any>([]);
+  const [totalLATE, setTotalLATE] = useState<number>(0);
+  const [LATE_EMP_LIST, setLATE_EMP_LIST] = useState<any>([]);
+  const [totalREST_TIME, setTotalREST_TIME] = useState<number>(0);
+  const [REST_TIME_EMP_LIST, setREST_TIME_EMP_LIST] = useState<any>([]);
+  const [totalVACATION, setTotalVACATION] = useState<number>(0);
+  const [VACATION_EMP_LIST, setVACATION_EMP_LIST] = useState<any>([]);
+  const [totalWORKING, setTotalWORKING] = useState<number>(0);
+  const [WORKING_EMP_LIST, setWORKING_EMP_LIST] = useState<any>([]);
+
   const toDay = moment().format('YYYY-MM-DD');
 
   const init = () => {
@@ -24,29 +34,86 @@ export default () => {
     });
     CALENDAR_DATA[toDay]?.map((i) => {
       let emp = empListTemp.find((emp) => emp.EMP_SEQ == i.EMP_ID);
-      emp['VACATION'] = i.VACATION ?? '0';
+      emp['EARLY'] = i.alear ?? '0';
+      emp['LATE'] = i.jigark ?? '0';
       emp['REST_TIME'] = i.REST_TIME;
+      emp['VACATION'] = i.VACATION ?? '0';
+      emp['IMAGE'] = i.IMAGE ?? '';
+
+      setTotalEARLY((totalEARLY) => totalEARLY + (i.alear === '1' ? 1 : 0));
+      setTotalLATE((totalLATE) => totalLATE + (i.jigark === '1' ? 1 : 0));
+      setTotalREST_TIME(
+        (totalREST_TIME) => totalREST_TIME + Number(i.REST_TIME),
+      );
+      setTotalVACATION(
+        (totalVACATION) => totalVACATION + (i.VACATION == '1' ? 1 : 0),
+      );
+
       i.CHANGE_START && i.CHANGE_END
         ? ((emp['START_TIME'] = i.CHANGE_START),
           (emp['END_TIME'] = i.CHANGE_END),
           (emp['WORKING'] = moment(i.CHANGE_END, 'kk:mm').diff(
             moment(i.CHANGE_START, 'kk:mm'),
-          )))
+          )),
+          setTotalWORKING(
+            (totalWORKING) =>
+              totalWORKING +
+              Number(
+                moment(i.CHANGE_END, 'kk:mm').diff(
+                  moment(i.CHANGE_START, 'kk:mm'),
+                ),
+              ),
+          ))
         : i.ATTENDANCE_TIME && i.WORK_OFF_TIME
         ? ((emp['START_TIME'] = i.ATTENDANCE_TIME),
           (emp['END_TIME'] = i.WORK_OFF_TIME),
           (emp['WORKING'] = moment(i.WORK_OFF_TIME, 'kk:mm').diff(
             moment(i.ATTENDANCE_TIME, 'kk:mm'),
-          )))
+          )),
+          setTotalWORKING(
+            (totalWORKING) =>
+              totalWORKING +
+              Number(
+                moment(i.WORK_OFF_TIME, 'kk:mm').diff(
+                  moment(i.ATTENDANCE_TIME, 'kk:mm'),
+                ),
+              ),
+          ))
         : ((emp['START_TIME'] = i.START),
           (emp['END_TIME'] = i.END),
           (emp['WORKING'] = moment(i.END, 'kk:mm').diff(
             moment(i.START, 'kk:mm'),
-          )));
-      emp['EARLY'] = i.alear ?? '0';
-      emp['LATE'] = i.jigark ?? '0';
+          )),
+          setTotalWORKING(
+            (totalWORKING) =>
+              totalWORKING +
+              Number(moment(i.END, 'kk:mm').diff(moment(i.START, 'kk:mm'))),
+          ));
     });
-    setEmpList(empListTemp);
+    setEMP_LIST(empListTemp);
+    setEARLY_EMP_LIST(
+      empListTemp.sort((a, b) =>
+        a.EARLY > b.EARLY ? -1 : b.EARLY > a.EARLY ? 1 : 0,
+      ),
+    );
+
+    setLATE_EMP_LIST(
+      empListTemp.sort((a, b) =>
+        a.LATE > b.LATE ? -1 : b.LATE > a.LATE ? 1 : 0,
+      ),
+    );
+
+    setREST_TIME_EMP_LIST(
+      empListTemp.sort((a, b) =>
+        a.REST_TIME > b.REST_TIME ? -1 : b.REST_TIME > a.REST_TIME ? 1 : 0,
+      ),
+    );
+
+    setVACATION_EMP_LIST(
+      empListTemp.sort((a, b) =>
+        a.VACATION > b.VACATION ? -1 : b.VACATION > a.VACATION ? 1 : 0,
+      ),
+    );
   };
 
   useEffect(() => {
@@ -55,9 +122,17 @@ export default () => {
 
   return (
     <DailyDashBoardScreenPresenter
-      empList={empList}
-      toDay={toDay}
-      data={CALENDAR_DATA[toDay]}
+      EMP_LIST={EMP_LIST}
+      totalEARLY={totalEARLY}
+      EARLY_EMP_LIST={EARLY_EMP_LIST}
+      totalLATE={totalLATE}
+      LATE_EMP_LIST={LATE_EMP_LIST}
+      totalREST_TIME={totalREST_TIME}
+      REST_TIME_EMP_LIST={REST_TIME_EMP_LIST}
+      totalVACATION={totalVACATION}
+      VACATION_EMP_LIST={VACATION_EMP_LIST}
+      totalWORKING={totalWORKING}
+      WORKING_EMP_LIST={WORKING_EMP_LIST}
     />
   );
 };
--- src/screens/LoggedInScreen/DashBoard/DailyDashBoardScreen/DailyDashBoardScreenPresenter.tsx
+++ src/screens/LoggedInScreen/DashBoard/DailyDashBoardScreen/DailyDashBoardScreenPresenter.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import Ripple from 'react-native-material-ripple';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+import DonutCard from '~/components/DonutCard';
+import FastImage from 'react-native-fast-image';
+
+interface IColor {
+  color: string;
+}
+interface ICard {
+  color: string;
+  isLast?: boolean;
+}
 
 const BackGround = styled.SafeAreaView`
   flex: 1;
@@ -25,35 +41,342 @@ const Section = styled.View`
   margin-bottom: 20px;
 `;
 
-export default ({empList, toDay, data}) => {
+const Card = styled(Ripple)<ICard>`
+  justify-content: flex-start;
+  align-items: center;
+  width: 200px;
+  height: 480px;
+  border-radius: 20px;
+  background-color: white;
+  margin-left: 20px;
+  margin-right: ${(props) => (props.isLast ? wp('100%') - 220 : 0)};
+`;
+
+const TitleWord = styled.Text<IColor>`
+  color: ${(props) => props.color};
+  align-self: flex-start;
+  font-weight: bold;
+  font-size: 22px;
+  margin-top: 20px;
+  margin-left: 20px;
+  font-size: 20px;
+`;
+
+const DodnutTextContainer = styled.View`
+  top: 115px;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PercentageText = styled.Text<IColor>`
+  color: ${(props) => props.color ?? 'black'};
+  font-size: 28px;
+  font-weight: bold;
+`;
+
+const PercentageSubText = styled.Text<IColor>`
+  color: ${(props) => props.color ?? 'black'};
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const EmpCard = styled.View`
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+`;
+
+const Bold = styled.Text`
+  font-weight: bold;
+`;
+
+const Column = styled.View`
+  flex-direction: column;
+`;
+
+const EmpConatainer = styled.View`
+  width: 100%;
+  height: 180px;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export default ({
+  EMP_LIST,
+  totalEARLY,
+  EARLY_EMP_LIST,
+  totalLATE,
+  LATE_EMP_LIST,
+  totalREST_TIME,
+  REST_TIME_EMP_LIST,
+  totalVACATION,
+  VACATION_EMP_LIST,
+  totalWORKING,
+  WORKING_EMP_LIST,
+}) => {
   return (
     <BackGround>
-      {console.log('empList', empList)}
-
       <ScrollView
+        style={{paddingBottom: hp('100%') - 420}}
         keyboardDismissMode="on-drag"
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{alignItems: 'center'}}>
-        <Container>
-          <Section>
+        <Container></Container>
+
+        <ScrollView
+          horizontal
+          snapToInterval={220}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}>
+          {/* <Card
+            onPress={() => console.log('index')}
+            rippleColor={'#666'}
+            rippleDuration={600}
+            rippleSize={1700}
+            rippleContainerBorderRadius={20}
+            rippleOpacity={0.1}>
             <Text>출근미체크율 = API 대기중</Text>
-          </Section>
-          <Section>
+          </Card>
+          <Card
+            onPress={() => console.log('index')}
+            rippleColor={'#666'}
+            rippleDuration={600}
+            rippleSize={1700}
+            rippleContainerBorderRadius={20}
+            rippleOpacity={0.1}>
             <Text>퇴근미체크율 = API 대기중</Text>
-          </Section>
-          <Section>
-            <Text>지각률 = {toDay}</Text>
-          </Section>
-          <Section>
-            <Text>조퇴률 = {toDay}</Text>
-          </Section>
-          <Section>
-            <Text>평균 휴게시간 = {data.length}</Text>
-          </Section>
-          <Section>
-            <Text>휴가중인 직원 = {toDay}</Text>
-          </Section>
-        </Container>
+          </Card> */}
+          <Card
+            onPress={() => console.log('index')}
+            rippleColor={'#666'}
+            rippleDuration={600}
+            rippleSize={1700}
+            rippleContainerBorderRadius={20}
+            rippleOpacity={0.1}>
+            <TitleWord color={'red'}>지각률</TitleWord>
+            <DonutCard
+              percentage={(totalLATE / EMP_LIST.length) * 100}
+              color={'red'}
+              max={100}
+            />
+            <DodnutTextContainer>
+              <PercentageText color={'red'}>
+                {(totalLATE / EMP_LIST.length) * 100}%
+              </PercentageText>
+              <PercentageSubText color={'red'}>{totalLATE}명</PercentageSubText>
+            </DodnutTextContainer>
+            <TitleWord color={'red'}>금일 지각 직원</TitleWord>
+            <EmpConatainer>
+              {LATE_EMP_LIST.filter((i) => i.LATE !== '0').length === 0 ? (
+                <Text style={{marginTop: 20}}>금일 지각 직원이 없습니다. </Text>
+              ) : (
+                LATE_EMP_LIST.map(
+                  (i) =>
+                    i.LATE !== '0' && (
+                      <EmpCard>
+                        <FastImage
+                          style={{
+                            margin: 10,
+                            marginLeft: 20,
+                            width: 40,
+                            height: 40,
+                            borderRadius: 20,
+                          }}
+                          source={{
+                            uri: `http://133.186.210.223/uploads/${i.IMAGE}`,
+                            headers: {Authorization: 'someAuthToken'},
+                            priority: FastImage.priority.low,
+                          }}
+                          resizeMode={FastImage.resizeMode.cover}
+                        />
+                        <Column>
+                          <Bold>
+                            {i.EMP_NAME} [
+                            {i.IS_MANAGER == '1' ? '매니저' : '스태프'}]
+                          </Bold>
+                        </Column>
+                      </EmpCard>
+                    ),
+                )
+              )}
+            </EmpConatainer>
+          </Card>
+          <Card
+            onPress={() => console.log('index')}
+            rippleColor={'#666'}
+            rippleDuration={600}
+            rippleSize={1700}
+            rippleContainerBorderRadius={20}
+            rippleOpacity={0.1}>
+            <TitleWord color={'red'}>조퇴률</TitleWord>
+            <DonutCard
+              percentage={(totalEARLY / EMP_LIST.length) * 100}
+              color={'red'}
+              max={100}
+            />
+            <DodnutTextContainer>
+              <PercentageText color={'red'}>
+                {(totalEARLY / EMP_LIST.length) * 100}%
+              </PercentageText>
+              <PercentageSubText color={'red'}>
+                {totalEARLY}명
+              </PercentageSubText>
+            </DodnutTextContainer>
+            <TitleWord color={'red'}>금일 조퇴 직원</TitleWord>
+            <EmpConatainer>
+              {EARLY_EMP_LIST.filter((i) => i.EARLY !== '0').length === 0 ? (
+                <Text style={{marginTop: 20}}>금일 조퇴 직원이 없습니다. </Text>
+              ) : (
+                EARLY_EMP_LIST.map(
+                  (i) =>
+                    i.EARLY !== '0' && (
+                      <EmpCard>
+                        <FastImage
+                          style={{
+                            margin: 10,
+                            marginLeft: 20,
+                            width: 40,
+                            height: 40,
+                            borderRadius: 20,
+                          }}
+                          source={{
+                            uri: `http://133.186.210.223/uploads/${i.IMAGE}`,
+                            headers: {Authorization: 'someAuthToken'},
+                            priority: FastImage.priority.low,
+                          }}
+                          resizeMode={FastImage.resizeMode.cover}
+                        />
+                        <Column>
+                          <Bold>
+                            {i.EMP_NAME} [
+                            {i.IS_MANAGER == '1' ? '매니저' : '스태프'}]
+                          </Bold>
+                        </Column>
+                      </EmpCard>
+                    ),
+                )
+              )}
+            </EmpConatainer>
+          </Card>
+          <Card
+            onPress={() => console.log('index')}
+            rippleColor={'#666'}
+            rippleDuration={600}
+            rippleSize={1700}
+            rippleContainerBorderRadius={20}
+            rippleOpacity={0.1}>
+            <TitleWord color={'red'}>평균 휴게시간</TitleWord>
+            <DonutCard
+              percentage={totalREST_TIME / EMP_LIST.length}
+              color={'red'}
+              max={60}
+            />
+            <DodnutTextContainer>
+              <PercentageText color={'red'} style={{marginTop: 10}}>
+                {totalREST_TIME / EMP_LIST.length}분
+              </PercentageText>
+            </DodnutTextContainer>
+            <TitleWord color={'red'}>휴게시간 상위직원</TitleWord>
+            <EmpConatainer>
+              {REST_TIME_EMP_LIST.filter((i) => i.REST_TIME !== '0').length ===
+              0 ? (
+                <Text style={{marginTop: 20}}>
+                  휴게시간이 있는 직원이 없습니다.
+                </Text>
+              ) : (
+                REST_TIME_EMP_LIST.map(
+                  (i) =>
+                    i.REST_TIME !== '0' && (
+                      <EmpCard>
+                        <FastImage
+                          style={{
+                            margin: 10,
+                            marginLeft: 20,
+                            width: 40,
+                            height: 40,
+                            borderRadius: 20,
+                          }}
+                          source={{
+                            uri: `http://133.186.210.223/uploads/${i.IMAGE}`,
+                            headers: {Authorization: 'someAuthToken'},
+                            priority: FastImage.priority.low,
+                          }}
+                          resizeMode={FastImage.resizeMode.cover}
+                        />
+                        <Column>
+                          <Bold>
+                            {i.EMP_NAME} [
+                            {i.IS_MANAGER == '1' ? '매니저' : '스태프'}]
+                          </Bold>
+                          <Text style={{marginTop: 5}}>{i.REST_TIME}분</Text>
+                        </Column>
+                      </EmpCard>
+                    ),
+                )
+              )}
+            </EmpConatainer>
+          </Card>
+          <Card
+            isLast={true}
+            onPress={() => console.log('index')}
+            rippleColor={'#666'}
+            rippleDuration={600}
+            rippleSize={1700}
+            rippleContainerBorderRadius={20}
+            rippleOpacity={0.1}>
+            <TitleWord color={'red'}>휴가중인 직원</TitleWord>
+            <DonutCard
+              percentage={totalVACATION}
+              color={'red'}
+              max={EMP_LIST.length}
+            />
+            <DodnutTextContainer>
+              <PercentageText color={'red'} style={{marginTop: 10}}>
+                {totalVACATION}명
+              </PercentageText>
+            </DodnutTextContainer>
+            <TitleWord color={'red'}>금일 휴가 직원</TitleWord>
+            <EmpConatainer>
+              {VACATION_EMP_LIST.filter((i) => i.VACATION !== '0').length ===
+              0 ? (
+                <Text style={{marginTop: 20}}>
+                  금일 휴가중인 직원이 없습니다.{' '}
+                </Text>
+              ) : (
+                VACATION_EMP_LIST.map(
+                  (i) =>
+                    i.VACATION !== '0' && (
+                      <EmpCard>
+                        <FastImage
+                          style={{
+                            margin: 10,
+                            marginLeft: 20,
+                            width: 40,
+                            height: 40,
+                            borderRadius: 20,
+                          }}
+                          source={{
+                            uri: `http://133.186.210.223/uploads/${i.IMAGE}`,
+                            headers: {Authorization: 'someAuthToken'},
+                            priority: FastImage.priority.low,
+                          }}
+                          resizeMode={FastImage.resizeMode.cover}
+                        />
+                        <Column>
+                          <Bold>
+                            {i.EMP_NAME} [
+                            {i.IS_MANAGER == '1' ? '매니저' : '스태프'}]
+                          </Bold>
+                        </Column>
+                      </EmpCard>
+                    ),
+                )
+              )}
+            </EmpConatainer>
+          </Card>
+        </ScrollView>
       </ScrollView>
     </BackGround>
   );
