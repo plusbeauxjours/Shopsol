79b8cc8701d0e1307cfb8daf468225ab9e4c0106
plusbeauxjours <plusbeauxjours@gmail.com>
2020-12-10 11:31:14 +0700

    Before android folder test

--- App.tsx
+++ App.tsx
@@ -30,6 +30,7 @@ export default codePush(codePushOptions)(App);
 
 // Andrioid
 // shopsol.master-gmail.com/ShopsolWesop
+// appcenter codepush release-react -a shopsol.master-gmail.com/ShopsolWesop -d Production
 // ┌────────────┬───────────────────────────────────────┐
 // │ Name       │ Key                                   │
 // ├────────────┼───────────────────────────────────────┤
@@ -40,10 +41,11 @@ export default codePush(codePushOptions)(App);
 
 // iOS
 // shopsol.master-gmail.com/ShopsolWesop-1
+// appcenter codepush release-react -a shopsol.master-gmail.com/ShopsolWesop-1 -d Production
 // ┌────────────┬───────────────────────────────────────┐
 // │ Name       │ Key                                   │
 // ├────────────┼───────────────────────────────────────┤
-// │ Production │ hIYDcLdpv4thrTbH-5OU0_hTcvrviP7Y-Ayr4 │
-// ├────────────┼───────────────────────────────────────┤
 // │ Staging    │ iy8-rDLfoik1pPaDhjd1iuUDlEApqhMcVmN55 │
+// ├────────────┼───────────────────────────────────────┤
+// │ Production │ hIYDcLdpv4thrTbH-5OU0_hTcvrviP7Y-Ayr4 │
 // └────────────┴───────────────────────────────────────┘
