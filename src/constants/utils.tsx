92ac8bdb65b2136f12a7e72850cb0cd09ac94e8f
plusbeauxjours <plusbeauxjours@plusbeauxjours.local>
2021-03-09 17:09:29 +0700

    SET 출퇴근시 위위치 권한 확인 후 위치 설정 타이밍

--- src/constants/utils.tsx
+++ src/constants/utils.tsx
@@ -71,6 +71,7 @@ export default {
           if (granted === PermissionsAndroid.RESULTS.GRANTED) {
             Geolocation.getCurrentPosition(
               (position) => {
+                handle(true);
                 setLat(position.coords.latitude);
                 setLong(position.coords.longitude);
               },
@@ -85,7 +86,6 @@ export default {
                 showLocationDialog: true,
               },
             );
-            handle(true);
           } else {
             Alert.alert(
               '위치정보 권한 거절',
